94567b22ec9d2a697637ea6c21532dda9b7aa8f7
Pranjal Gupta <guptapranjal3105@gmail.com>
2024-11-23 08:52:21 +0530

    Replacing the text fields with drop down for methods and sources

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -10,52 +10,78 @@ const AddExpense = () => {
   const [exp_date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
 
   const ADD_EXPENSE_MUTATION = gql`
-    mutation addExpense($amount: Float!, $description: String!, $method_id: Int!,  $source_id: Int!, $exp_date: String!) 
-    {
-        expense: createExpense(amount: $amount, description: $description, method_id: $method_id, source_id: $source_id, date: $exp_date) 
-        {
-            id,
-            amount,
-            description,
-            method_id,
-            method {
-              name
-            }
-            source_id,
-            source {
-              name
-            }
-            date,
-            created_by,
-            updated_by
+    mutation addExpense(
+      $amount: Float!
+      $description: String!
+      $method_id: Int!
+      $source_id: Int!
+      $exp_date: String!
+    ) {
+      expense: createExpense(
+        amount: $amount
+        description: $description
+        method_id: $method_id
+        source_id: $source_id
+        date: $exp_date
+      ) {
+        id
+        amount
+        description
+        method_id
+        method {
+          name
         }
-    }`;
+        source_id
+        source {
+          name
+        }
+        date
+        created_by
+        updated_by
+      }
+    }
+  `;
 
   // eslint-disable-next-line no-unused-vars
-  const [createExpense, { data, loading, error }] = useMutation(ADD_EXPENSE_MUTATION);
+  const [createExpense, { data, loading, error }] =
+    useMutation(ADD_EXPENSE_MUTATION);
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent the default form submission
     try {
       // Execute the GraphQL mutation with the form data
-      await createExpense({ variables: { amount: parseFloat(amount), description, method_id: parseInt(method_id), source_id: parseInt(source_id), exp_date } });
+      await createExpense({
+        variables: {
+          amount: parseFloat(amount),
+          description,
+          method_id: parseInt(method_id),
+          source_id: parseInt(source_id),
+          exp_date,
+        },
+      });
 
       // Reset form state after submission
       setAmount(0.0);
-      setDescription('');
+      setDescription("");
       setMethod(0);
       setSource(0);
       setDate(moment(new Date()).format("YYYY-MM-DD"));
     } catch (err) {
-      console.error('Error creating user:', err);
+      console.error("Error creating user:", err);
     }
   };
 
   return (
     <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="amount">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="amount"
+          >
             Amount
           </label>
           <input
@@ -69,7 +95,10 @@ const AddExpense = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="description"
+          >
             Description
           </label>
           <input
@@ -82,8 +111,12 @@ const AddExpense = () => {
             required
           />
         </div>
+        {/*
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="method_id">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="method_id"
+          >
             Method Id
           </label>
           <input
@@ -95,8 +128,8 @@ const AddExpense = () => {
             onChange={(e) => setMethod(e.target.value)}
             required
           />
-        </div>
-        <div className="mb-4">
+        </div>*/}
+        {/*<div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="source_id">
             Source Id
           </label>
@@ -109,9 +142,59 @@ const AddExpense = () => {
             onChange={(e) => setSource(e.target.value)}
             required
           />
+        </div>*/}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="method_id"
+          >
+            Method
+          </label>
+          <select
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="method_id"
+            value={method_id}
+            onChange={(e) => setMethod(e.target.value)}
+            required
+          >
+            <option value="">Select Method</option>
+            {/* Replace these options with your actual method IDs */}
+            <option value="2">In-Cash</option>
+            <option value="3">Debit Card</option>
+            <option value="4">Credit Card</option>
+            <option value="5">UPI</option>
+            <option value="6">AC to AC</option>
+          </select>
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="doe">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="source_id"
+          >
+            Source Id
+          </label>
+          <select
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="source_id"
+            value={source_id}
+            onChange={(e) => setSource(e.target.value)}
+            required
+          >
+            <option value="">Select Source</option>
+            {/* Replace these options with your actual source IDs */}
+            <option value="1">KOTAK MAHINDRA BANK</option>
+            <option value="2">ICICI BANK</option>
+            <option value="3">SBI BANK</option>
+            <option value="7">HSBC BANK</option>
+            <option value="8">HDFC BANK</option>
+            <option value="9">CASH</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="doe"
+          >
             Date of Expense
           </label>
           <input
@@ -128,8 +211,10 @@ const AddExpense = () => {
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit" disabled={loading}>
-          {loading ? 'Submitting...' : 'Submit'}
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? "Submitting..." : "Submit"}
           </button>
         </div>
       </form>
