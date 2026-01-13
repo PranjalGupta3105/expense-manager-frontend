e1ad0420caf9760b7a022a1313adba831af5cf0d
Pranjal Gupta <guptapranjal3105@gmail.com>
2026-01-13 18:35:42 +0530

    Opening Data Ananlysis App in the react app

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import MonthlyExpenses from '../assets/MonthlyExpenses.png';
 
 const Dashboard = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-[200px] mt-10 sm:mt-20">
-      <img src={MonthlyExpenses} alt="Monthly Expenses" className="max-w-full h-auto" />
+      <iframe
+        src="http://localhost:8501/"
+        title="Expense Dashboard"
+        className="w-full h-[600px] border rounded"
+        allow="clipboard-write"
+      ></iframe>
     </div>
   );
 }
