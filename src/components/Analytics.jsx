ba509264842c1615f636a2bd2815924ff9293512
Pranjal Gupta <guptapranjal3105@gmail.com>
2026-02-04 10:08:04 +0530

    Dashboard Page renamed to Analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Dashboard = () => {
+const Analytics = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-[200px] mt-10 sm:mt-20">
       <iframe
         src="http://localhost:8501/"
-        title="Expense Dashboard"
+        title="Expense Analytics"
         className="w-full h-[600px] border rounded"
         allow="clipboard-write"
       ></iframe>
@@ -13,4 +13,4 @@ const Dashboard = () => {
   );
 }
 
-export default Dashboard
+export default Analytics
