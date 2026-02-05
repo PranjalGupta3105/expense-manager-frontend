fcb7875b1edf1dbce163200e0699eec160b15644
Pranjal Gupta <guptapranjal3105@gmail.com>
2026-02-05 21:29:28 +0530

    Pick value from env rather then the CI/CD script

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Analytics = () => {
+  const ANALYTICS_URL = import.meta.env.VITE_ANALYTICS_URL ?? "http://localhost:8501/";
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-[200px] mt-10 sm:mt-20">
       <iframe
-        src="http://localhost:8501/"
+        src={ANALYTICS_URL}
         title="Expense Analytics"
         className="w-full h-[600px] border rounded"
         allow="clipboard-write"
